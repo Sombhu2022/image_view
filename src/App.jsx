cfd63cf5a0bac4bef82f421917297e5549562a27
saniyaj mallik <saniyajmallik12@gmail.com>
2023-10-11 09:27:28 +0530

    files and routes added of home and upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-
 import './App.css'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './pages/home/Home'
+import Upload from './pages/upload/Upload'
 
 function App() {
 
 
   return (
-    <>
-      <div>
-        image uploader using cloudinary....
-      </div>
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/upload' element={<Upload />}/>
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/pages/upload/Upload.jsx
@@ -0,0 +1,11 @@
+import './upload.scss';
+
+const Upload = () => {
+  return (
+    <>
+    <h2 className='upload-heading'> Select and upload Images</h2>
+    </>
+  )
+}
+
+export default Upload
