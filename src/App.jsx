be2012412f1c6e2bfff4534aa183db29e2e4bb7f
som <sombhud2637@gmail.com>
2023-10-12 09:56:48 +0530

    new add

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './pages/home/Home'
 import Upload from './pages/upload/Upload'
+import Option from './pages/options/Option'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/upload' element={<Upload />}/>
+        <Route path='/:id' element={<Option/>}/>
       </Routes>
     </Router>
   )
