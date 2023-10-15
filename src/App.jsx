4bd6e3f4126f880b414235d0d96e2f9a38cfe451
saniyaj mallik <saniyajmallik12@gmail.com>
2023-10-15 20:26:33 +0530

    layout updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import './App.css'
+
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './pages/home/Home'
-import Upload from './pages/upload/Upload'
+
 import Option from './pages/options/Option'
+import { Toaster } from "react-hot-toast";
 
 function App() {
 
 
   return (
     <Router>
+      <Toaster />
       <Routes>
         <Route path='/' element={<Home />}/>
-        <Route path='/upload' element={<Upload />}/>
         <Route path='/:id' element={<Option/>}/>
       </Routes>
     </Router>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,38 +1,56 @@
-import { useEffect, useState } from 'react';
-import './home.scss';
-import axios from 'axios';
-import { Link, useNavigate } from 'react-router-dom';
-
+import { useEffect, useState } from "react";
+import "./home.scss";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import Upload from "./components/upload/Upload";
 
 const Home = () => {
-  const [images, setImages] = useState([]);
-  const navigete = useNavigate()
+	const [images, setImages] = useState([]);
+	const navigete = useNavigate();
 
- const UploadImage = ()=>{
-      navigete('/upload') 
-   
- }
+	const UploadImage = () => {
+		navigete("/upload");
+	};
 
-  const getImages = async () => {
-    try {
-      const { data } = await axios.get(`http://localhost:8080/api/images`);
-      setImages(data);
-    } catch (error) {
-      console.error(error);
-    }
-  }
+	const getImages = async () => {
+		try {
+			const { data } = await axios.get(`http://localhost:8080/api/images`);
+			setImages(data);
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
-  useEffect(() => {
-    getImages();
-  }, []);
+	useEffect(() => {
+		getImages();
+	}, []);
 
-  const optionHandaler =()=>{
+	const optionHandaler = () => {};
 
-  }
+	return (
+		<div className='container'>
+			<div className='left'>
+				<Upload />
+			</div>
+			<div className='right' id='images'>
+				{images &&
+					images.allImages?.map((img, index) => (
+						<div className='data-container' key={index}>
+							<div>
+								<img src={img.url} className='show-image' />
+							</div>
+							<div>
+								<p>{img.caption}</p>
 
-  return (
-    <div>
-     <h1 className='home-heading'>Welcome to CloudGallery</h1>
+								<div>
+									<Link to={`/${img._id}`}>🌐</Link>
+								</div>
+							</div>
+						</div>
+					))}
+			</div>
+
+			{/* <h1 className='home-heading'>Welcome to CloudGallery</h1>
      <button name='add' className='addButton' onClick={UploadImage}>Upload New Image</button><br/>
 
       {images && images.allImages?.map((img, index) => (
@@ -49,9 +67,9 @@ const Home = () => {
         </div>
             
         </div>
-      ))}
-    </div>
-  );
-}
+      ))} */}
+		</div>
+	);
+};
 
 export default Home;
