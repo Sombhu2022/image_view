8b17bf934fcafac688ff7acc8d28282051b07638
saniyaj mallik <saniyajmallik12@gmail.com>
2023-12-06 16:52:30 +0530

    redux setup complete

--- src/pages/auth/login/Login.jsx
+++ src/pages/auth/login/Login.jsx
@@ -1,68 +1,79 @@
-import axios from 'axios';
-import React, { useState } from 'react'
-import './login.scss'
-import toast from 'react-hot-toast';
-import { Link , useNavigate } from 'react-router-dom';
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./login.scss";
+import toast from "react-hot-toast";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { loginUser, selectUser } from "../../../redux/slices/auth";
 
 function Login() {
-  const [user , setUser] = useState({});
-  const navigate = useNavigate()
-  const hendleData = (e)=>{
-        setUser({...user,[e.target.name]:e.target.value})
-   }
+	const [user, setUser] = useState({});
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const {isAuthenticated} = useSelector(selectUser)
 
-  const handleLogin =async (e)=>{
-     e.preventDefault();
-   if(Object.entries(user).length == 0){
-      return toast.error('plese input valid data....');
-   }
-   if(!user.password || !user.email){
-    return toast.error('email and password required....');
-   }
-   
-     try {
-      //const config = {headers:{'Content-type':'application/json'}, withCedentials:true}
-      const { data } = await axios.post('http://localhost:8080/auth/login', user, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        withCredentials: true,
-      })
-        toast.success(data.message)
-        console.log('success',data)
-        navigate('/')
-
-     } catch (error) {
-      
-      console.log(error)
-      toast.error(error.response.data.message)
-        
-     }
-  }
+	useEffect(() => {
+		
+		if (isAuthenticated === true) {
+			navigate("/");
+		}
+	}, [dispatch, isAuthenticated, navigate]);
 
 
-  return (
-    <div>
-      <form action="" className='auth-form' onSubmit={handleLogin}>
-      <b style={{fontSize:"25px"}}>Login form</b>
-      <input type="email" name="email" id="" placeholder='enter valid Email' onChange={hendleData} />
-      <input type="password" name="password" id="" placeholder='enter vaild password ' onChange={hendleData}/>
-      <button type='submit'>Log In</button>
-     
-      <p>If you loss your password
-         <strong>
-          <Link to={'/auth/forgotPassword'}><span >Forget password</span></Link>
-        </strong> 
-      </p>
-      <br />
-      <hr />
-      <b>If you are not Register  </b>
-      <Link to={'/auth/registretion'}><button type=''>Register</button></Link>
-      </form>
-  
-    </div>
-  )
+
+
+	const hendleData = (e) => {
+		setUser({ ...user, [e.target.name]: e.target.value });
+	};
+
+	const handleLogin = async (e) => {
+		e.preventDefault();
+		if (Object.entries(user).length == 0) {
+			return toast.error("plese input valid data....");
+		}
+		if (!user.password || !user.email) {
+			return toast.error("email and password required....");
+		}
+		dispatch(loginUser(user));
+	};
+
+	return (
+		<div>
+			<form action='' className='auth-form' onSubmit={handleLogin}>
+				<b style={{ fontSize: "25px" }}>Login form</b>
+				<input
+					type='email'
+					name='email'
+					id=''
+					placeholder='enter valid Email'
+					onChange={hendleData}
+				/>
+				<input
+					type='password'
+					name='password'
+					id=''
+					placeholder='enter vaild password '
+					onChange={hendleData}
+				/>
+				<button type='submit'>Log In</button>
+
+				<p>
+					If you loss your password
+					<strong>
+						<Link to={"/auth/forgotPassword"}>
+							<span>Forget password</span>
+						</Link>
+					</strong>
+				</p>
+				<br />
+				<hr />
+				<b>If you are not Register </b>
+				<Link to={"/auth/registretion"}>
+					<button type=''>Register</button>
+				</Link>
+			</form>
+		</div>
+	);
 }
 
-export default Login
+export default Login;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,105 +1,39 @@
 import { useEffect, useState } from "react";
 import "./home.scss";
-import axios from "axios";
-import { Link, useNavigate } from "react-router-dom";
-import Upload from "./components/upload/Upload";
-import toast from "react-hot-toast";
-import Bottom from '../lauout/bottom/Bottom'
+import Bottom from "../lauout/bottom/Bottom";
 import PhotosPage from "./components/view/PhotosPage";
 import TextsPage from "./components/view/TextsPage";
+import { useSelector } from "react-redux";
+import { selectUser } from "../../redux/slices/auth";
+import { useNavigate } from "react-router-dom";
 const Home = () => {
-	const [view, setView] = useState('p');
-
-	const toggleView =(param)=>{
-		setView(param)
-	}
-
-
-
-
-
-
-
-
-
-
-
-
-	
-	const navigete = useNavigate();
-
-	const UploadImage = () => {
-		navigete("/upload");
-	};
-
-	
-
-	const reloadPage = (reload) => {
-		if (reload) {
-			getImages();
+	const [view, setView] = useState("p");
+	const {isAuthenticated} = useSelector(selectUser)
+	const navigate = useNavigate()
+	useEffect(() => {
+		
+		if (isAuthenticated !== true) {
+			navigate("/auth/login");
 		}
-	};
+	}, [ isAuthenticated, navigate]);
 
-	
-	const optionHandaler = () => {};
-	const userLogout = async () => {
-		try {
-			const { data } = await axios.get(`http://localhost:8080/auth/logout`, {
-				headers: {
-					"Content-Type": "application/json",
-				},
-				withCredentials: true,
-			});
 
-			toast.success(data.message);
-			navigete("/auth/login");
-		} catch (error) {
-			console.error(error);
-		}
+	const toggleView = (param) => {
+		setView(param);
 	};
 
 	return (
-
 		<>
-		<div className="home-container">
-			<div className={`${view ==="p"? "open" :""} photo-container`}>
-				<PhotosPage />
+			<div className='home-container'>
+				<div className={`${view === "p" ? "open" : ""} photo-container`}>
+					<PhotosPage />
+				</div>
+				<div className={`${view === "t" ? "open" : ""} text-container`}>
+					<TextsPage />
+				</div>
 			</div>
-			<div className={`${view==="t"? "open" :""} text-container`}>
-				<TextsPage/>
-			</div>
-		</div>
-		<Bottom toggleView={toggleView}/>
+			<Bottom toggleView={toggleView} />
 		</>
-		// <div>
-		// 	<button className='logout-button' onClick={userLogout}>
-		// 		Logout
-		// 	</button>
-		// 	<div className='container'>
-		// 		{/* <div className='left'>
-		// 		<Upload  reloadParentPage={reloadPage} />
-		// 	</div> */}
-		// 		<div className='right' id='images'>
-		// 			{images &&
-		// 				images.allImages?.map((img, index) => (
-		// 					<div className='data-container' key={index}>
-		// 						<div>
-		// 							<img src={img.url} className='show-image' />
-		// 						</div>
-		// 						<div>
-		// 							<p>{img.caption}</p>
-
-		// 							<div className='option'>
-		// 								<Link className='link' to={`/${img._id}`}>
-		// 									🌐
-		// 								</Link>
-		// 							</div>
-		// 						</div>
-		// 					</div>
-		// 				))}
-		// 		</div>
-		// 	</div>
-		// </div>
 	);
 };
 
